4c03f4344bb59cc4b50c184e2f19fb0416ac0c62
Gilles Cherix <gilles.cherix@bluewin.ch>
2016-03-19 20:34:47 +0000

    Hook on select month

--- cockpit.js
+++ cockpit.js
@@ -84,22 +84,9 @@ if (Meteor.isClient) {
         }
     });
 
-    
-/* old stuff that has to do with the example app */
-    
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    'change select': function () {
+        
     }
   });
 }
